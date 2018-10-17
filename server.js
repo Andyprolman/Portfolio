246316aa952829a7744fa26f4b644d783e481140
Andrew Prolman <aprolman21@gmail.com>
2018-10-17 15:45:36 -0700

    personal profiling. uses bootstrap snd css, fade in on page load, and svg file

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+var profile = require('./profile')
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use('/profile', profile)
+
+app.use('/public', express.static('public'))
+
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const data = {
+        person: {
+            firstName: 'Andy',
+            lastName: 'Prolman'
+        }
+    }
+    res.render('index', data);
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.post('/thanks', (req, res) => {
+    console.log(req.body)
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+    to: 'aprolman21@gmail.com',
+    from: req.body.email,
+    subject: req.body.subject,
+    text: req.body.message,
+    html: req.body.message,
+};
+sgMail.send(msg);
+    res.render('thanks', {contact: req.body})
+});
+
+
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080')
+});
