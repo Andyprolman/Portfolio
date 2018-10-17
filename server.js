0f4c6d088801af5774ec805c161e183b33e1516d
Andrew Prolman <aprolman21@gmail.com>
2018-10-17 16:12:44 -0700

    fixes server port

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ sgMail.send(msg);
 
 
 
-const PORT = process.env.PORT || 8080;
+//const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+app.listen(process.env.PORT || 8080);
